ed977bb0d54a3f65047aadc3bd26cf7db6f8a834
Ayrton Lucena <ayrton.lucena7@gmail.com>
2020-12-26 15:55:38 -0300

    Nova alteração dia 26-12-2020

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
+import { useState } from 'react'
+const axios = require('axios')
+
 function Home() {
-    return <div>Eu amo minha estrelinha :P</div>
+
+    const [state, setState] = useState([])
+
+    const getTest = (e) => {
+        e.preventDefault()
+        axios.get('https://api.punkapi.com/v2/beers')
+            .then(res => setState(res.data) )
+            .catch(err => console.log(err))
+    }
+
+    return (
+    <div>
+        <p>Eu amo minha estrelinha :P</p>
+        <button onClick={getTest}>Testando</button>
+        {
+            state.length >= 1 ? state.map((beer, idx) => {
+                return <p key={idx}>{beer.name}</p>
+            })
+            : ''
+        }
+    </div>
+    )
 }
 
 export default Home
---
+++ pages/test.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default function Test() {
+    
+    function handleTestFunction() {
+        axios.post('/api/');
+    }
+}
